08f6e3be398edacb0803cd7d05343fc19f65f1a2
Vincent Beauvieux <vbeauvieux@gmail.com>
2018-10-14 20:04:08 +0200

    implemented createRequestAsPayer in hoc

--- src/containers/withRequest.tsx
+++ src/containers/withRequest.tsx
@@ -3,18 +3,48 @@ import * as React from "react";
 
 export interface InjectedRequestProps {
   requestNetworkProps: { [key: string]: any };
+  createRequestAsPayer: (onSuccess: (txhash: string) => void) => void;
+}
+
+export interface State {
+  requestNetworkProps?: any;
 }
 
 export const withRequest = <P extends InjectedRequestProps>(
   Component: React.ComponentType<P>
 ) =>
-  class WithRequest extends React.Component<P> {
+  class WithRequest extends React.Component<P, State> {
+    private requestNetworkInstance?: any;
+
+    createRequestAsPayer = (onSuccess: (txhash: string) => void) => {
+      this.requestNetworkInstance.requestEthereumService
+        .createRequestAsPayer(
+          ["0x6f179c0B2782932AdC62F871023DC14C1F695d91"],
+          [0.01],
+          undefined,
+          undefined,
+          undefined,
+          JSON.stringify({}),
+          undefined,
+          undefined,
+          { gasPrice: "15000000000" }
+        )
+        .then(({ transaction }: any) => {
+          onSuccess(transaction.hash);
+        });
+    };
+
     render() {
       return (
         <Consumer>
           {requestNetwork => {
+            this.requestNetworkInstance = requestNetwork.requestNetwork;
             return (
-              <Component {...this.props} requestNetworkProps={requestNetwork} />
+              <Component
+                {...this.props}
+                requestNetworkProps={requestNetwork}
+                createRequestAsPayer={this.createRequestAsPayer}
+              />
             );
           }}
         </Consumer>

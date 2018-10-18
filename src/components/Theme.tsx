44a48697358e211ba07e07cfdcf242de03e42172
Vincent Beauvieux <vbeauvieux@gmail.com>
2018-10-18 20:51:22 +0200

    added some elements to theme and improved validation for amount textinput

--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -47,7 +47,22 @@ export const Title2 = styled.h2`
   font-size: 1.8em;
 `;
 
-export const Text = styled.p`
+interface PProps {
+  color: string;
+}
+const p: StyledFunction<PProps & React.HTMLProps<HTMLParagraphElement>> =
+  styled.p;
+export const Text = p`
   font-family: "Raleway", sans-serif;
   color: ${props => props.color || "#4d4d4d"};
+`;
+
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  color: #777;
+  font-family: "Raleway", sans-serif;
+  font-size: 0.8em;
+  margin: 0.5em 0;
+  position: relative;
 `;
--- src/containers/Employer/EmployerForm.tsx
+++ src/containers/Employer/EmployerForm.tsx
@@ -3,8 +3,8 @@ import * as React from "react";
 import * as Yup from "yup";
 //@ts-ignore
 import * as Web3 from "web3";
-import { createStyles } from "@material-ui/core";
-import { Form, Input, Button } from "../../components/Theme";
+// import { createStyles } from "@material-ui/core";
+import { Form, Input, Button, Label, Text } from "../../components/Theme";
 
 interface FormValues {
   address: string;
@@ -18,34 +18,51 @@ interface FormProps {
 
 const InnerForm: React.SFC<
   InjectedFormikProps<FormProps, FormValues>
-> = props => (
-  <Form onSubmit={props.handleSubmit} style={styles.root}>
-    <Input
-      id="address"
-      placeholder="Employee Ethereum address"
-      type="text"
-      onChange={props.handleChange}
-      value={props.values.address}
-      style={styles.textInput}
-      border={props.errors.address && "1px solid red"}
-    />
-    {props.touched.address &&
-      props.errors.address && <div>{props.errors.address}</div>}
-    <Input
-      id="amount"
-      placeholder="Amount"
-      type="text"
-      onChange={props.handleChange}
-      value={props.values.amount}
-      style={styles.textInput}
-    />
-    {props.touched.amount &&
-      props.errors.amount && <div>{props.errors.amount}</div>}
-    <Button type="submit" disabled={props.isSubmitting}>
-      Submit
-    </Button>
-  </Form>
-);
+> = props => {
+  console.log(props);
+
+  return (
+    <Form onSubmit={props.handleSubmit}>
+      <Label>
+        <Input
+          id="address"
+          placeholder="Employee Ethereum address"
+          type="text"
+          onChange={props.handleChange}
+          value={props.values.address}
+          border={
+            props.touched.address
+              ? props.errors.address && "1px solid red"
+              : undefined
+          }
+        />
+        {props.touched.address &&
+          props.errors.address && (
+            <Text color="red">{props.errors.address}</Text>
+          )}
+      </Label>
+      <Label>
+        <Input
+          id="amount"
+          placeholder="Amount"
+          type="text"
+          onChange={props.handleChange}
+          value={props.values.amount}
+          border={
+            props.touched.amount
+              ? props.errors.amount && "1px solid red"
+              : undefined
+          }
+        />
+        {props.touched.amount &&
+          props.errors.amount && <Text color="red">{props.errors.amount}</Text>}
+      </Label>
+      <Button type="submit" disabled={props.isSubmitting}>
+        Submit
+      </Button>
+    </Form>
+  );
+};
 
 const EmployerForm = withFormik<FormProps, FormValues>({
   mapPropsToValues: () => ({ address: "", amount: "" }),
@@ -57,7 +74,11 @@ const EmployerForm = withFormik<FormProps, FormValues>({
         return Web3.utils.isAddress(value);
       }
     ),
-    amount: Yup.number()
+    amount: Yup.string().test("isValidAmount", "Invalid amount", function(
+      value
+    ) {
+      return !Number.isNaN(+value);
+    })
   }),
   handleSubmit: (values, { setSubmitting }) => {
     setTimeout(() => {
@@ -67,24 +88,24 @@ const EmployerForm = withFormik<FormProps, FormValues>({
   }
 })(InnerForm);
 
-const styles = createStyles({
-  root: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "flex-start",
-    alignItems: "center",
-    minWidth: 340
-  },
-  label: {
-    marginTop: 40,
-    marginBottom: 20
-  },
-  textInput: {
-    marginBottom: 20
-  },
-  button: {
-    margin: 0
-  }
-});
+// const styles = createStyles({
+// root: {
+//   // display: "flex",
+//   // flexDirection: "column",
+//   // justifyContent: "flex-start",
+//   // alignItems: "center",
+//   minWidth: 340
+// },
+// label: {
+//   marginTop: 40,
+//   marginBottom: 20
+// },
+// textInput: {
+//   marginBottom: 20
+// },
+// button: {
+//   margin: 0
+// }
+// });
 
 export default EmployerForm;

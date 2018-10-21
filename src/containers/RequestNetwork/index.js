ce2af811a12ac2958d5462c8abaa5c8d8a19ad37
Vincent Beauvieux <vbeauvieux@gmail.com>
2018-10-21 19:06:53 +0200

    avoid issue on safari

--- src/containers/RequestNetwork/index.js
+++ src/containers/RequestNetwork/index.js
@@ -1,13 +1,13 @@
-var _a;
 import * as React from "react";
 import RequestNetworkProvider from "./Provider";
-export var Provider = ((_a = React.createContext({
-    isReady: false,
-    networkMismatch: false,
-    currentNetwork: "",
-    currentAccount: "",
-    requestNetwork: undefined
-  })),
-  _a.Provider),
-  Consumer = _a.Consumer;
+
+const ctx = React.createContext({
+  isReady: false,
+  networkMismatch: false,
+  currentNetwork: "",
+  currentAccount: "",
+  requestNetwork: undefined
+});
+export const Consumer = ctx.Consumer;
+export const Provider = ctx.Provider;
 export default RequestNetworkProvider;
